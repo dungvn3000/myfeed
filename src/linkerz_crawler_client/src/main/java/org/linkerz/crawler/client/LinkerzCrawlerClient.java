ade7813976945b023d5a40c49a7c4de057dde675
Nguyen Duc Dung <dungvn3000@gmail.com>
2012-07-04 01:36:00 +0800

    Add linkerz_crawler_client module.

---
+++ src/linkerz_crawler_client/src/main/java/org/linkerz/crawler/client/LinkerzCrawlerClient.java
@@ -0,0 +1,37 @@
+package org.linkerz.crawler.client;
+
+import com.hazelcast.client.ClientConfig;
+import com.hazelcast.client.HazelcastClient;
+import org.linkerz.crawler.core.job.Job;
+
+/**
+ * The Class LinkerzCrawlerClient.
+ *
+ * @author Nguyen Duc Dung
+ * @since 7/4/12, 1:25 AM
+ */
+public final class LinkerzCrawlerClient {
+
+    private static HazelcastClient client;
+
+    private LinkerzCrawlerClient() {
+    }
+
+    public static LinkerzCrawlerClient connect(String ipAddress, String userName, String passWord) {
+        if (client == null) {
+            ClientConfig clientConfig = new ClientConfig();
+            clientConfig.getGroupConfig().setName(userName).setPassword(passWord);
+            clientConfig.addAddress(ipAddress);
+            client = HazelcastClient.newHazelcastClient(clientConfig);
+        }
+        return new LinkerzCrawlerClient();
+    }
+
+    public void addJob(Job job) {
+        client.getQueue("jobQueue").add(job);
+    }
+
+    public void shutdown() {
+        client.shutdown();
+    }
+}
---
+++ src/linkerz_crawler_client/src/main/java/org/linkerz/crawler/client/Main.java
@@ -0,0 +1,19 @@
+package org.linkerz.crawler.client;
+
+import org.linkerz.crawler.core.job.CrawlJob;
+
+/**
+ * The Class Main.
+ *
+ * @author Nguyen Duc Dung
+ * @since 7/4/12, 1:25 AM
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        LinkerzCrawlerClient client = LinkerzCrawlerClient.connect("127.0.0.1", "dev", "dev");
+        client.addJob(new CrawlJob("http://vnexpress.net/"));
+        client.shutdown();
+    }
+
+}
