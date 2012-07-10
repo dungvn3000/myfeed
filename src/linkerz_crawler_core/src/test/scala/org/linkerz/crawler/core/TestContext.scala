33c018daf1925e2d5e8755488b324e53940fdad8
Nguyen Duc Dung <dungvn3000@gmail.com>
2012-07-11 01:27:03 +0800

    add Async Http Client library framework.

---
+++ src/linkerz_crawler_core/src/test/scala/org/linkerz/crawler/core/TestContext.scala
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 2012 - 2013 LinkerZ
+ */
+
+package org.linkerz.crawler.core
+
+import org.junit.runner.RunWith
+import org.scalatest.FunSuite
+import org.springframework.test.context.ContextConfiguration
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
+import org.springframework.context.{ApplicationContext, ApplicationContextAware}
+
+/**
+ * The Class TestContext.
+ *
+ * @author Nguyen Duc Dung
+ * @since 7/10/12, 1:04 AM
+ *
+ */
+
+@ContextConfiguration(locations = Array("/context.xml"))
+@RunWith(classOf[SpringJUnit4ClassRunner])
+class TestContext extends FunSuite with ApplicationContextAware {
+
+  var context: ApplicationContext = _
+
+  test("testContext") {
+    assert(context != null)
+  }
+
+  def setApplicationContext(applicationContext: ApplicationContext) {
+    context = applicationContext
+  }
+}
