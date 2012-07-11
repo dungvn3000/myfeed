312765b3478215057ba1482dba4af94b1ff536fe
Nguyen Duc Dung <dungvn3000@gmail.com>
2012-07-11 15:06:57 +0800

    Remove Dispatch.

--- src/linkerz_crawler_core/src/test/scala/org/linkerz/crawler/core/TestHttpClient.scala
+++ src/linkerz_crawler_core/src/test/scala/org/linkerz/crawler/core/TestHttpClient.scala
@@ -8,11 +8,6 @@ import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
 import org.scalatest.FunSuite
 import com.twitter.util.Time
-import org.apache.http.impl.client.{BasicResponseHandler, DefaultHttpClient}
-import org.apache.http.client.methods.HttpGet
-import org.apache.http.params.{HttpProtocolParamBean, BasicHttpParams, CoreProtocolPNames}
-import org.apache.http.HttpVersion
-import dispatch.{:/, Http}
 import com.ning.http.client._
 
 /**
@@ -26,44 +21,11 @@ import com.ning.http.client._
 @RunWith(classOf[JUnitRunner])
 class TestHttpClient extends FunSuite {
 
-  test("testSimpleDownload") {
-
-    val params = new BasicHttpParams()
-    val paramsBean = new HttpProtocolParamBean(params)
-    paramsBean.setVersion(HttpVersion.HTTP_1_1)
-    paramsBean.setContentCharset("UTF-8")
-    paramsBean.setUseExpectContinue(false)
-
-    params.setParameter(CoreProtocolPNames.USER_AGENT,
-      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.7; rv:10.0.2) Gecko/20100101 Firefox/10.0.2")
-
-    params.setBooleanParameter("http.protocol.handle-redirects", false)
-
-    val httpClient = new DefaultHttpClient()
-    httpClient.setParams(params)
-
-    val httpGet = new HttpGet("http://vnexpress.net/")
-
-
-    val responseHandler = new BasicResponseHandler()
-    val time = Time.now
-    val responseBody = httpClient.execute(httpGet, responseHandler)
-    println(Time.now.inMilliseconds - time.inMilliseconds + "ms")
-
-    //    println(responseBody.toString())
-  }
-
-  test("testDispatch") {
-    val http = new Http
-    val request = :/("vnexpress.net")
-//    val handler = request >>> System.out
-//    http(handler)
-  }
-
   test("testAsyncHttpClient") {
     val cf = new AsyncHttpClientConfig.Builder()
-    cf.setUserAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10.7; rv:10.0.2) Gecko/20100101 Firefox/10.0.2")
-    val asyncHttpClient = new AsyncHttpClient(cf.build())
+      .setUserAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10.7; rv:10.0.2) Gecko/20100101 Firefox/10.0.2")
+      .build()
+    val asyncHttpClient = new AsyncHttpClient(cf)
     val f = asyncHttpClient.prepareGet("http://vnexpress.net").execute(new AsyncCompletionHandler[Response] {
       def onCompleted(response: Response) = {
         println(response.getStatusCode)
