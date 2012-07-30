379e41d6a64acd53ada6a604140fd9defd3a40ae
Nguyen Duc Dung <dungvn3000@gmail.com>
2012-07-30 21:53:35 +0800

    Implement linkerz server.

---
+++ src/linkerz_server/src/main/scala/org/linkerz/server/Main.scala
@@ -0,0 +1,21 @@
+/*
+ * Copyright (C) 2012 - 2013 LinkerZ
+ */
+
+package org.linkerz.server
+
+import org.springframework.context.support.GenericXmlApplicationContext
+
+/**
+ * The Class Main.
+ *
+ * @author Nguyen Duc Dung
+ * @since 7/30/12, 8:59 PM
+ *
+ */
+
+object Main extends App {
+
+  val context = new GenericXmlApplicationContext("application.xml")
+
+}
