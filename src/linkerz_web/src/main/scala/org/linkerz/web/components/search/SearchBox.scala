057102138f97bc4079b23996005108af30af5f17
Nguyen Duc Dung <dungvn3000@gmail.com>
2012-08-03 03:02:18 +0800

    Add twitter bootstrap framework.

---
+++ src/linkerz_web/src/main/scala/org/linkerz/web/components/search/SearchBox.scala
@@ -0,0 +1,17 @@
+/*
+ * Copyright (C) 2012 - 2013 LinkerZ
+ */
+
+package org.linkerz.web.components.search
+
+/**
+ * The Class SearchBox.
+ *
+ * @author Nguyen Duc Dung
+ * @since 8/3/12, 2:00 AM
+ *
+ */
+
+class SearchBox {
+
+}
