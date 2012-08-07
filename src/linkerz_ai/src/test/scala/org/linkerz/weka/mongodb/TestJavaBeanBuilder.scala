8a24654e840d8a090d83885a9d655d1c1e60ae43
Nguyen Duc Dung <dungvn3000@gmail.com>
2012-08-07 21:10:06 +0800

    update test case.

--- src/linkerz_ai/src/test/scala/org/linkerz/weka/mongodb/TestJavaBeanBuilder.scala
+++ src/linkerz_ai/src/test/scala/org/linkerz/weka/mongodb/TestJavaBeanBuilder.scala
@@ -5,7 +5,6 @@
 package org.linkerz.weka.mongodb
 
 import org.scalatest.FunSuite
-import weka.core.Instance
 import collection.mutable.ListBuffer
 import weka.classifiers.trees.J48
 import weka.classifiers.Evaluation
@@ -56,4 +55,39 @@ class TestJavaBeanBuilder extends FunSuite {
     println(evaluation.toSummaryString)
   }
 
+
+  test("testPredic") {
+    val weathers = new ListBuffer[Weather]
+    for (i <- 1 to 10) {
+      val weather = new Weather
+      weather.outlook = "sunny"
+      weather.temperature = 85
+      weather.humidity = 85
+      weather.windy = "false"
+      weather.play = "yes"
+
+      weathers += weather
+    }
+
+    val train = JavaBeanBuilder.build(classOf[Weather], weathers.toList)
+    train.setClassIndex(train.numAttributes() - 1)
+    val cls = new J48
+    cls.buildClassifier(train)
+
+    val weather = new Weather
+    weather.outlook = "sunny"
+    weather.temperature = 85
+    weather.humidity = 85
+    weather.windy = "false"
+
+    val weatherTest = List[Weather](weather)
+
+    val test = JavaBeanBuilder.build(classOf[Weather], weatherTest)
+    test.setClassIndex(test.numAttributes() - 1)
+
+    val predict = cls.classifyInstance(test.instance(0))
+
+    assert(test.classAttribute().value(predict.toInt) == "yes")
+
+  }
 }
