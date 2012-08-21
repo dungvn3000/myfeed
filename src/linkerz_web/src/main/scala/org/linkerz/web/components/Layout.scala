229f04a4adf15790f3351ca511b73411853a2225
Nguyen Duc Dung <dungvn3000@gmail.com>
2012-08-21 14:46:29 +0800

    Upgrade bootstrap to 2.1.0

--- src/linkerz_web/src/main/scala/org/linkerz/web/components/Layout.scala
+++ src/linkerz_web/src/main/scala/org/linkerz/web/components/Layout.scala
@@ -19,18 +19,7 @@ import org.apache.tapestry5.annotations.{Import, Property}
   "context:assets/css/subnav.css",
   "context:assets/bootstrap/css/bootstrap.css"
 ), library = Array(
-  "context:assets/bootstrap/js/bootstrap-transition.js",
-  "context:assets/bootstrap/js/bootstrap-alert.js",
-  "context:assets/bootstrap/js/bootstrap-modal.js",
-  "context:assets/bootstrap/js/bootstrap-dropdown.js",
-  "context:assets/bootstrap/js/bootstrap-scrollspy.js",
-  "context:assets/bootstrap/js/bootstrap-tab.js",
-  "context:assets/bootstrap/js/bootstrap-tooltip.js",
-  "context:assets/bootstrap/js/bootstrap-popover.js",
-  "context:assets/bootstrap/js/bootstrap-button.js",
-  "context:assets/bootstrap/js/bootstrap-collapse.js",
-  "context:assets/bootstrap/js/bootstrap-carousel.js",
-  "context:assets/bootstrap/js/bootstrap-typeahead.js"
+  "context:assets/bootstrap/js/bootstrap.js"
 ))
 class Layout {
 
