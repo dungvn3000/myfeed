87256458b91c0ef276b02d7e24d70d357f9025d4
Nguyen Duc Dung <dungvn3000@gmail.com>
2012-08-23 04:29:27 +0800

    Add ManualTest Category for Test Unit will not be run by Maven.

---
+++ src/linkerz_test/src/main/scala/org/linkerz/test/categories/ManualTest.scala
@@ -0,0 +1,16 @@
+/*
+ * Copyright (C) 2012 - 2013 LinkerZ
+ */
+
+package org.linkerz.test.categories
+
+/**
+ * The interface to mark the test unit will not run by maven.
+ *
+ * @author Nguyen Duc Dung
+ * @since 8/23/12, 4:25 AM
+ *
+ */
+trait ManualTest {
+
+}
