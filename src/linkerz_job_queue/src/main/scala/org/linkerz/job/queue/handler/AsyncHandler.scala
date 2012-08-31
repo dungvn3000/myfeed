6936a507474d1ef36c1160e6fc6ea133f1afd76e
dungvn3000@gmail.com <ducdung2512>
2012-09-01 01:39:19 +0700

    Change waiting recheck time.

--- src/linkerz_job_queue/src/main/scala/org/linkerz/job/queue/handler/AsyncHandler.scala
+++ src/linkerz_job_queue/src/main/scala/org/linkerz/job/queue/handler/AsyncHandler.scala
@@ -92,9 +92,9 @@ abstract class AsyncHandler[J <: Job, S <: Session[J]] extends HandlerInSession[
     while (!isStop) {
       //Check all worker if they are free, finish the job.
       if (workers.filter(worker => !worker.isFree).size == 0) {
-        //waiting for 10s and recheck again
+        //waiting for 3s and recheck again
         info("All worker are free, waiting for 10s and recheck")
-        Thread.sleep(1000 * 10)
+        Thread.sleep(1000 * 3)
         if (workers.filter(worker => !worker.isFree).size == 0) {
           logger.info("It seem is no more job and all workers is free. Finish....")
           isStop = true
