76a6bb192a0da3b167dc9a11a820c6ad0b62eff4
dungvn3000@gmail.com <ducdung2512>
2012-09-02 17:17:25 +0700

    Implement RabbitMQController.

--- src/linkerz_job_queue/src/main/scala/org/linkerz/job/queue/controller/RabbitMQController.scala
+++ src/linkerz_job_queue/src/main/scala/org/linkerz/job/queue/controller/RabbitMQController.scala
@@ -4,10 +4,10 @@
 
 package org.linkerz.job.queue.controller
 
-import com.rabbitmq.client.{FlowListener, QueueingConsumer, ConnectionFactory}
+import com.rabbitmq.client.{QueueingConsumer, ConnectionFactory}
 import util.Marshal
 import org.linkerz.job.queue.core.Job
-import actors.DaemonActor
+import actors.Actor
 
 /**
  * This controller will reivce job from RabbitMQ server.
@@ -21,23 +21,18 @@ class RabbitMQController extends BaseController {
   var connectionFactory: ConnectionFactory = _
   var queueName = "jobQueue"
 
-  val consumerActor = new DaemonActor() {
+  val consumerActor = new Actor() {
     def act() {
       val connection = connectionFactory.newConnection()
       val channel = connection.createChannel()
       val consumer = new QueueingConsumer(channel)
-      channel.basicConsume(queueName, true, consumer)
+      channel.basicConsume(queueName, false, consumer)
       loop {
         val delivery = consumer.nextDelivery()
         if (delivery.getBody != null) {
           val job = Marshal.load[Job](delivery.getBody)
           handlerActor ! NEXT(job)
-        }
-        receive {
-          case STOP => {
-            reply("exit")
-            exit()
-          }
+          channel.basicAck(delivery.getEnvelope.getDeliveryTag, false)
         }
       }
     }
@@ -46,10 +41,5 @@ class RabbitMQController extends BaseController {
   override def start() {
     super.start()
     consumerActor.start()
-  }
-
-  override def stop() {
-    super.stop()
-    consumerActor !? STOP
   }
 }
--- src/linkerz_job_queue/src/test/scala/org/linkerz/job/queue/TestRabbitMQController.scala
+++ src/linkerz_job_queue/src/test/scala/org/linkerz/job/queue/TestRabbitMQController.scala
@@ -5,10 +5,10 @@
 package org.linkerz.job.queue
 
 import controller.RabbitMQController
-import handler.{EchoHandler, SyncHandler}
-import job.{EchoJob, SumJob}
+import handler.EchoHandler
+import job.EchoJob
 import org.junit.Test
-import com.rabbitmq.client.ConnectionFactory
+import com.rabbitmq.client.{MessageProperties, ConnectionFactory}
 import util.Marshal
 
 /**
@@ -21,6 +21,7 @@ import util.Marshal
 class TestRabbitMQController {
 
   val factory = new ConnectionFactory
+  factory.setHost("localhost")
 
   @Test
   def testBasicJob() {
@@ -30,15 +31,40 @@ class TestRabbitMQController {
     controller.start()
 
     //Send a job to server.
-    factory.setHost("localhost")
     val connection = factory.newConnection()
     val channel = connection.createChannel()
-    channel.queueDeclare("jobQueue", false, false, false, null)
-    channel.basicPublish("", "jobQueue", null, Marshal.dump(new EchoJob("Hello Rabbit")))
+    channel.queueDeclare("jobQueue", false, false, true, null)
+    channel.basicPublish("", "jobQueue", MessageProperties.PERSISTENT_BASIC,
+      Marshal.dump(new EchoJob("Hello Rabbit")))
     channel.close()
     connection.close()
 
     Thread.sleep(1000)
+    controller.stop()
+  }
+
+  @Test
+  def testWith1000Job() {
+    val controller = new RabbitMQController
+    controller.connectionFactory = factory
+    controller.handlers = List(new EchoHandler)
+    controller.start()
+
+    //Send a job to server.
+    val connection = factory.newConnection()
+    val channel = connection.createChannel()
+    channel.queueDeclare("jobQueue", false, false, true, null)
+
+    for (i <-0 to 999) {
+      channel.basicPublish("", "jobQueue", MessageProperties.PERSISTENT_BASIC,
+        Marshal.dump(new EchoJob("Hello Rabbit " + i)))
+    }
+
+    channel.close()
+    connection.close()
+
+    //Waiting for the controller.
+    Thread.sleep(3000)
     controller.stop()
   }
 
