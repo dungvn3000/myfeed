4bf166c94a7c3acb086de5e7894f3f415f977320
dungvn3000@gmail.com <ducdung2512>
2012-11-02 08:28:24 +0700

    Using sbt instead of maven.

--- project/Build.scala
+++ project/Build.scala
@@ -1,9 +1,27 @@
 import sbt._
 import Keys._
+import Project._
 
 object LinkerZBuild extends Build {
 
-  lazy val LinkerZ = Project("LinkerZ", file("."))
+  lazy val linkerZ = Project("linkerz", file("."), settings = defaultSettings)
+
+  lazy val linkerZCore = Project("linkerz_core", file("src/linkerz_core"), settings = defaultSettings).settings(
+    libraryDependencies ++= coreDependencies
+  )
+
+  lazy val linkerZTest = Project("linkerz_test", file("src/linkerz_test"), settings = defaultSettings).settings(
+    libraryDependencies ++= testDependencies
+  ).dependsOn(linkerZCore)
+
+  val coreDependencies = Seq(
+    "commons-collections" % "commons-collections" % "3.2.1",
+    "commons-digester" % "commons-digester" % "2.1"
+  )
+
+  val testDependencies = Seq(
+    "junit" % "junit" % "4.10"
+  )
 
 }
 
