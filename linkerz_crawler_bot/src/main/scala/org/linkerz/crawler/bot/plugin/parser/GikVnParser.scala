8080cd097834380463a53e905a3b8051cb46b959
dungvn3000@gmail.com <ducdung2512>
2012-11-13 21:14:12 +0700

    Add Gik.vn parser.

---
+++ linkerz_crawler_bot/src/main/scala/org/linkerz/crawler/bot/plugin/parser/GikVnParser.scala
@@ -0,0 +1,28 @@
+package org.linkerz.crawler.bot.plugin.parser
+
+import org.linkerz.crawler.bot.plugin.{ParserData, NewsParser}
+
+/**
+ * The Class GikVnParser.
+ *
+ * @author Nguyen Duc Dung
+ * @since 11/13/12 9:05 PM
+ *
+ */
+class GikVnParser extends NewsParser {
+
+  def pluginData = {
+    val pluginData = new ParserData
+    pluginData.name = "Gik.vn"
+    pluginData.version = "0.0.1"
+    pluginData.group = "startup"
+    pluginData.pluginClass = this.getClass.getName
+    pluginData.enable = true
+    pluginData.urlRegex = "*/gik.vn/*"
+    pluginData.titleSelection = ".main-view h1.paper-header"
+    pluginData.contentSelection = ".main-view .paper-main"
+    pluginData.imgSelection = ".main-view .paper-main img"
+    pluginData
+  }
+
+}
