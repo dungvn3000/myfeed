89836566e662e56ea04df7e8be51c1c6990cf890
dungvn3000@gmail.com <ducdung2512>
2012-11-29 23:24:02 +0700

    Remove example.

--- linkerz_storm/src/main/scala/org/linkerz/storm/Main.scala
+++ linkerz_storm/src/main/scala/org/linkerz/storm/Main.scala
@@ -1,10 +1,5 @@
 package org.linkerz.storm
 
-import backtype.storm.topology.TopologyBuilder
-import bolt.{WordCount, SplitSentence}
-import spout.RandomSentenceSpout
-import backtype.storm.tuple.Fields
-import backtype.storm.{LocalCluster, Config}
 
 /**
  * The Class Main.
@@ -14,18 +9,5 @@ import backtype.storm.{LocalCluster, Config}
  *
  */
 object Main extends App {
-  val builder = new TopologyBuilder()
 
-  builder.setSpout("randsentence", new RandomSentenceSpout)
-  builder.setBolt("split", new SplitSentence, 8).shuffleGrouping("randsentence")
-  builder.setBolt("count", new WordCount, 12).fieldsGrouping("split", new Fields("word"))
-
-  val conf = new Config()
-  conf setDebug true
-
-  val cluster = new LocalCluster()
-  cluster.submitTopology("test", conf, builder.createTopology())
-  Thread sleep 10000
-  cluster.killTopology("test")
-  cluster.shutdown()
 }
