237725be737bff3fe081af019268b94bb6ba229d
dungvn3000@gmail.com <ducdung2512>
2012-12-16 18:41:34 +0700

    Not using scala test to run a topology in local mode.

---
+++ linkerz_crawl_topology/src/test/scala/org/linkerz/crawl/topology/TopologyLocalRunner.scala
@@ -0,0 +1,31 @@
+package org.linkerz.crawl.topology
+
+import backtype.storm.{Config, LocalCluster}
+import backtype.storm.utils.Utils
+import CrawlTopology._
+
+/**
+ * The Class MainFunSuite.
+ *
+ * @author Nguyen Duc Dung
+ * @since 11/30/12 3:59 AM
+ *
+ */
+class TopologyLocalRunner extends App {
+
+  val localCluster = new LocalCluster
+
+  val conf = new Config
+  conf setDebug false
+  conf setNumWorkers 8
+  //Set time out for an crawl job is fifty minutes
+  conf setMessageTimeoutSecs 60 * 15
+
+  localCluster.submitTopology("crawler", conf, topology)
+
+  //One day.
+  Utils sleep 1000 * 60 * 60 * 24
+
+  localCluster.shutdown()
+
+}
---
+++ linkerz_recommendation_topology/src/test/scala/org/linkerz/recommendation/TopologyLocalRunner.scala
@@ -0,0 +1,28 @@
+package org.linkerz.recommendation
+
+import backtype.storm.{Config, LocalCluster}
+import backtype.storm.utils.Utils
+import org.linkerz.recommendation.RecommendationTopology._
+
+/**
+ * The Class MainFunSuite.
+ *
+ * @author Nguyen Duc Dung
+ * @since 12/16/12 4:38 AM
+ *
+ */
+class TopologyLocalRunner extends App {
+
+  val localCluster = new LocalCluster
+
+  val conf = new Config
+  conf setDebug true
+
+  localCluster.submitTopology("recommendation", conf, topology)
+
+  //One day.
+  Utils sleep 1000 * 60 * 60 * 24
+
+  localCluster.shutdown()
+
+}
