78cd6f0cd43576f5ef62cd30f91e7489be031a3e
Phan Hồng Phúc <phanhongphucit@gmail.com>
2013-02-21 18:15:07 +0700

    Remove isLike property

--- linkerz_model/src/main/scala/org/linkerz/model/Rating.scala
+++ linkerz_model/src/main/scala/org/linkerz/model/Rating.scala
@@ -6,11 +6,17 @@ package org.linkerz.model
 
 import org.bson.types.ObjectId
 
+/**
+ * Rating stand for Care the link or Like the comment.
+ * @param _id
+ * @param targetId the id of link or comment base on targetType
+ * @param userId
+ * @param targetType 0 if Care, 1 if Comment
+ */
 case class Rating(
                    _id: ObjectId = new ObjectId,
                    targetId: ObjectId,
                    userId: ObjectId,
-                   isLiked: Boolean,
                    targetType: Int
                    ) extends LinkerZModel(_id)
 
