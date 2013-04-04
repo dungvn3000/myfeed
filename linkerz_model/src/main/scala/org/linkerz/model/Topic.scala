342ae523fe11377a61692f3adb31617eba0d39e3
dungvn3000@gmail.com <ducdung2512>
2013-04-04 12:55:03 +0700

    Rename Group to Topic.

--- linkerz_model/src/main/scala/org/linkerz/model/Topic.scala
+++ linkerz_model/src/main/scala/org/linkerz/model/Topic.scala
@@ -3,13 +3,13 @@ package org.linkerz.model
 import org.bson.types.ObjectId
 
 /**
- * The Class Group.
+ * The Class Topic.
  *
  * @author Nguyen Duc Dung
  * @since 3/29/13 12:37 PM
  *
  */
-case class Group(
+case class Topic(
                   _id: ObjectId = new ObjectId(),
                   userId: ObjectId,
                   name: String
