f4bac28f9f9fd2a9e6a1fd7de9faf67cc7bf2a91
dungvn3000@gmail.com <ducdung2512>
2013-05-08 00:49:08 +0700

    Apply Http-Client

--- linkerz_topology_crawl/src/main/scala/org/linkerz/crawl/topology/factory/DownloadFactory.scala
+++ linkerz_topology_crawl/src/main/scala/org/linkerz/crawl/topology/factory/DownloadFactory.scala
@@ -5,12 +5,12 @@
 package org.linkerz.crawl.topology.factory
 
 import org.linkerz.crawl.topology.downloader.DefaultDownloader
-import org.apache.http.impl.conn.tsccm.ThreadSafeClientConnManager
 import org.apache.http.conn.ssl.SSLSocketFactory
 import org.apache.http.conn.scheme.{SchemeRegistry, PlainSocketFactory, Scheme}
 import org.apache.http.params.{BasicHttpParams, CoreProtocolPNames, CoreConnectionPNames}
 import org.linkerz.crawl.topology.downloader.handler.StrictlyRedirectStrategy
 import org.apache.http.impl.client.DefaultHttpClient
+import org.apache.http.impl.conn.PoolingClientConnectionManager
 
 /**
  * The Class DefaultDownloadFactory.
@@ -24,13 +24,14 @@ object DownloadFactory {
 
   val httpParams = new BasicHttpParams()
   httpParams.setParameter(CoreProtocolPNames.USER_AGENT, FireFoxUserAgent.value)
-  //Set time out 30s
-  httpParams.setParameter(CoreConnectionPNames.SO_TIMEOUT, 1000 * 30)
+  //Set time out 10s
+  httpParams.setParameter(CoreConnectionPNames.SO_TIMEOUT, 1000 * 10)
+  httpParams.setParameter(CoreConnectionPNames.CONNECTION_TIMEOUT, 1000 * 10)
 
-  val schemeRegistry: SchemeRegistry = new SchemeRegistry
+  val schemeRegistry = new SchemeRegistry
   schemeRegistry.register(new Scheme("http", 80, PlainSocketFactory.getSocketFactory))
   schemeRegistry.register(new Scheme("https", 443, SSLSocketFactory.getSocketFactory))
-  val cm = new ThreadSafeClientConnManager(schemeRegistry)
+  val cm = new PoolingClientConnectionManager(schemeRegistry)
   cm.setMaxTotal(20000)
   cm.setDefaultMaxPerRoute(500)
 
