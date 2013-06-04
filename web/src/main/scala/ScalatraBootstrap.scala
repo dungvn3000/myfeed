f0f80f1203c3c1e6b4eacc2a758911dbdf258f6d
dungvn3000@gmail.com <ducdung2512>
2013-06-04 19:23:43 +0700

    give up on scalatra.

--- web/src/main/scala/ScalatraBootstrap.scala
+++
@@ -1,9 +0,0 @@
-import org.scalatra._
-import javax.servlet.ServletContext
-import vn.myfeed.web.controller.{FlowersSwagger, ResourcesApp, MyFeed}
-
-class ScalatraBootstrap extends LifeCycle {
-  override def init(context: ServletContext) {
-    context.mount(new MyFeed, "/*")
-  }
-}
